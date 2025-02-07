5e992b2f5dd20b1c0f8a1cc5e3ac19c8daccba78
Leandro Borges <leandroborges_br@hotmail.com>
2025-02-07 10:00:20 -0500

    Trigger

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "public_lb_dns" {
-  value = aws_lb.main.dns_name
-}
+# output "public_lb_dns" {
+#   value = aws_lb.main.dns_name
+# }
 
-output "webapp_instance0_public_ip" {
-  value = aws_instance.main[0].public_ip
-}
+# output "webapp_instance0_public_ip" {
+#   value = aws_instance.main[0].public_ip
+# }
 
-output "private_key_pem" {
-  value = nonsensitive(module.ssh_keys.private_key_pem)
-}
+# output "private_key_pem" {
+#   value = nonsensitive(module.ssh_keys.private_key_pem)
+# }
 
 output "data" {
   value = data.tfe_outputs.networking.nonsensitive_values
